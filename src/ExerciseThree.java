480d4254dcc3b5eb3c5c58a5f78f9a9df3489940
ethomascappello <ethomascappello@Marist-HNSQXQ2.necsd.net>
2019-06-17 18:19:20 -0400

    Exercise Three

---
+++ src/ExerciseThree.java
@@ -0,0 +1,12 @@
+
+public class ExerciseThree {
+
+	public static void main(String[] args) {
+		System.out.println("    J      A      V    V     A");
+        System.out.println("    J     A A      V  V     A A");
+        System.out.println("J   J    AAAAA      VV     AAAAA");
+        System.out.println( "J J    A     A      V    A     A");
+
+	}
+
+}
